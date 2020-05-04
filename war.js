78e1993a7267550b5318a2ab922b375a7e8bb614
Christopher Birkenhagen <c.birkenhagen@gmail.com>
2020-05-04 12:58:56 -0400

    Game Complete and bug free. Changed to take 3 cards per War

--- war.js
+++ war.js
@@ -1,20 +1,20 @@
 //@@@@@@@@@@@@@@@@@@@@@@ Classes @@@@@@@@@@@@@@@@@@@@@@@@@@@
-function Card(rank, face, frontUrl, backUrl) {
+function Card(score, suit, rank, frontUrl, backUrl) {
+    this.score = score;
     this.rank = rank;
-    this.face = face;
+    this.suit = suit;
     this.frontUrl = frontUrl;
     this.backUrl = backUrl;
 }
 function Game(gameNum, count, p1Length, p2Length, kittyLength, numTies, winner) {
         this.gameNum = gameNum;
-        this.counter = count;
-        this.playerONE = p1Length;
-        this.playerTWO = p2Length;
+        this.count = count;
+        this.p1Length = p1Length;
+        this.p2Length = p2Length;
         this.kittyLength = kittyLength;
         this.numTies = numTies;
         this.winner = winner;
 }
-
 
 //@@@@@@@@@@@@@@@@@@@@@@@ Game Object @@@@@@@@@@@@@@@@@@@@@@@@@@@
 const war = { 
@@ -31,36 +31,27 @@ const war = {
     gameNum: 0,  //the game currently running
     numTies: 0,
     numTurns: 0,
+    numCardsInTie: 2, //
 
-    //fill 'deck' with cards specified by suits & numRanks
+    //--------======= INITIALIZE THE GAME =========---------
+
+
+    //FILL THE DECK: fill 'deck' with cards specified by suits & numRanks
     buildDeck: function () {
-        //specify the suits
+        //specify the card inputs
         const suits = ['clubs','spades','hearts','diamonds']
-        //add ranks to array
-        const numRanks = 13  //specify the number of ranks
-        const ranks = []
-        for (let i=1;i<=numRanks;i++) {  
-            ranks.push(i)
-        }
+        const scores = [2,3,4,5,6,7,8,9,10,11,12,13,14]
+        const ranks = ['2 ','3 ','4 ','5 ','6 ','7 ','8 ','9 ','10','J ','Q ','K ','A ']
+                
         //add Cards to 'deck', one for each 'rank' in each 'suit'
         for (let i=0; i<suits.length; i++) {
-            for (let j=0;j<ranks.length;j++) {
-                this.deck.push(new Card(j+1, suits[i]))
+            for (let j=0;j<scores.length;j++) {
+                this.deck.push(new Card(scores[j], suits[i], ranks[j]))
             }
         }        
     },
-    //shuffle the array indices from received array
-    // shuffle: function() {
-    //     let temp, random;
-    //     for (let i = this.deck.length; i!==0; i--) {
-    //         //pick an index at random
-    //         random = Math.floor(Math.random() * i);        
-    //         // And swap it with the current element.
-    //         temp = this.deck[i-1];
-    //         this.deck[i-1] = this.deck[random];
-    //         this.deck[random] = temp;
-    //     }
-    // },
+
+    //SHUFFLE AN ARRAY
     shuffle: function(arrayToShuffle) {
         let temp, random;
         for (let i = arrayToShuffle.length; i!==0; i--) {
@@ -72,7 +63,8 @@ const war = {
             arrayToShuffle[random] = temp;
         }
     },
-    //split the deck between p1Deck and p2Deck, dealing from the end.
+
+    //DEAL CARDS TO EACH PLAYER: split the deck between p1Deck and p2Deck, dealing from the end.
     dealCards: function() {
         while (this.deck.length !== 0) {
             this.p1Deck.push(this.deck.pop())
@@ -80,17 +72,16 @@ const war = {
         }
         
     },
-    //returns a Card from the front of an array of Cards
+
+    //--------======= INITIALIZE THE GAME =========---------
+
+    //PULL A CARD FROM PLAYER DECK: returns a Card from the front of an array of Cards
     getCard: function(playerDeck) {return playerDeck.shift()
     },
-    //pushes all the card in play to thedeck received (p1Deck/p2Deck) 
+
+    //GIVE WINNER ALL CARDS ON THE TABLE: pushes all the card in play to thedeck received (p1Deck/p2Deck) 
     winner: function(turnWinner) {
-        // console.log("winner Started")
-        // winner.push(this.firstCard, this.secondCard)
-        //     if (this.kitty !== []) {
-        //         for (let item in this.kitty)
-        //             winner.push(this.kitty[item])
-        //     }
+        
         this.fillKitty()
         this.shuffle(this.kitty)
         for (let item in this.kitty) turnWinner.push(this.kitty[item])
@@ -98,39 +89,98 @@ const war = {
         this.secondCard = null
         this.kitty = []
     },
-    //loads the kitty array with the first&second Card, if 'switch=true' takes 1 card from each player deck.
+
+   
+
+    //COMPARE THE CARDS FOR WIN/LOSS/TIE: compare firstCard & secondCard and send to winner or to kitty if tie.
+    compareCards: function(showTurns) {
+        // console.log("compareCards Started")
+        let turnWinner, takeExtraCard = false
+        if (this.firstCard.score > this.secondCard.score) this.winner(this.p1Deck)
+        else if (this.secondCard.score > this.firstCard.score) this.winner(this.p2Deck)
+        else {
+            takeExtra = true
+            if (showTurns) console.log("<<<<<<<<<<<<<<<<<<<<<< WAR!  TIME TO TAKE NAMES AND CHEW BUBBLEGUM!!! >>>>>>>>>>>>>>>>>>>>>>>>>")
+            this.numTies++
+            this.fillKitty(takeExtra)
+        }
+    },
+    //GATHER UP TABLE CARDS: loads the kitty array with the first&second Card, if 'switch=true' takes 1 card from each player deck.
     fillKitty: function (takeExtraCard=0) {
         // console.log("fillKitty Started")
         if (this.firstCard !== null) this.kitty.push(this.firstCard)
         if (this.secondCard !== null) this.kitty.push(this.secondCard)
         if (takeExtraCard) {
-            if (this.p1Deck.length > 0) this.kitty.push(this.p1Deck.shift())
-            if (this.p2Deck.length > 0) this.kitty.push(this.p2Deck.shift())
+            for (let i=0; i<this.numCardsInTie; i++) {
+                if (this.p1Deck.length > 0) this.kitty.push(this.p1Deck.shift())
+                if (this.p2Deck.length > 0) this.kitty.push(this.p2Deck.shift())
+            }
         }
         this.firstCard = null;
         this.secondCard = null;
     },
-    //compare firstCard & secondCard and send to winner or to kitty if tie.
-    compareCards: function(showTurns) {
-        // console.log("compareCards Started")
-        let turnWinner, takeExtraCard = false
-        if (this.firstCard.rank > this.secondCard.rank) this.winner(this.p1Deck)
-        else if (this.secondCard.rank > this.firstCard.rank) this.winner(this.p2Deck)
-        else {
-            takeExtraCard = true
-            if (showTurns) console.log("There was a tie, players add one to the kitty")
-            this.numTies++
-            this.fillKitty(takeExtraCard)
-        }
-    },
-    //check for empty player decks
+
+    //CHECK IF PLAYER DECKS ARE EMPTY
     testForWin: function() {
         // console.log("testForWin Started")
         if (this.p1Deck.length === 0) return "Player One"
         else if (this.p2Deck.length === 0) return "Player Two"
         else return null
     },
-    //The actual turn mechanic. Flips cards to compare until one player has zero cards remaining.
+    //clears all values in preparation for a game
+    initialize: function() {
+        this.deck = []
+        this.p1Deck = []
+        this.p2Deck = []
+        this.kitty = []
+        this.firstCard = null
+        this.secondCard = null
+        this.counter = 0
+        this.numTies = 0
+        this.gameWinner = null
+        this.gameOver = false
+        this.buildDeck()
+        this.shuffle(this.deck)
+        this.dealCards()
+    },
+
+    //---------------===================== REPORTING ================-----------------
+
+    printWinner: function() {
+        console.log("---------==============@@@@@@@@@@@@@@@@ WINNER @@@@@@@@@@@@@@@==============---------")
+        console.log(`---------==============   CONGRATULATIONS  ${this.gameWinner.toUpperCase()}     ==============---------`)
+        console.log(`---------==============     Turns: ${this.counter} | # Wars: ${this.numTies}    ==============---------`)
+    },
+
+    printGameState: function() {
+        console.log(`== ${this.gameNum} ---TURN ${this.counter} | P1 CARD: ${this.firstCard.rank} | P2 CARD: ${this.secondCard.rank} | CARDS: P1 / P2 - Kitty: ${this.p1Deck.length} / ${this.p2Deck.length} - ${this.kitty.length}`)
+    },
+    //saves the game information to 'savedGames'
+    logGame: function() {
+
+        //game, count, p1Length, p2Length, kittyLength
+        let game = new Game(this.gameNum, this.counter, this.p1Deck.length, this.p2Deck.length, this.kitty.length, this.numTies, this.gameWinner)
+        this.savedGames.push(game)
+        this.numTurns += this.counter
+        // console.log(JSON.stringify(game))
+    },
+    //generates stats on the game set
+    runStats: function() {
+        let aveTies = Math.floor(this.numTurns/this.numTies)
+        let mean = this.numTurns/this.gameNum
+
+        console.log("------------============== GAME SET METRICS ==============------------")
+        console.log(`GAMES: ${this.gameNum} | TOTAL TURNS ${this.numTurns} | AVE TURNS/GAME ${mean},`)
+        console.log(`TOTAL TIES: ${this.numTies} | AVE TURN PER TIE ${aveTies}`) 
+        
+        for (let each in this.savedGames) {
+            console.log(`@@@ GAME: ${this.savedGames[each].gameNum} | # TURNS: ${this.savedGames[each].count} | CARDS: P1 / P2 - Kitty: ${this.savedGames[each].p1Length} / ${this.savedGames[each].p2Length} - ${this.savedGames[each].kittyLength} | # WARS ${this.savedGames[each].numTies} | WINNER ${this.savedGames[each].winner} @@@`)
+        }
+
+    },
+    //-----------------------================ GAME PLAY ===============--------------------------
+
+    //THE TURN MECHANIC: Flips cards to compare until one player has zero cards remaining.
     flipFight: function(showTurns) {
 
         while (!this.gameOver) {
@@ -152,41 +202,14 @@ const war = {
             //end while loop if a player deck is empty
             this.gameWinner = this.testForWin()
             if (this.gameWinner) {
-                console.log("@@@@@@@@@@@@@@@@@@@WINNER@@@@@@@@@@@@@@@@@@")
-                console.log(`........CONGRATULATIONS ${this.gameWinner}`)
+                this.printWinner()
                 this.gameOver = true
             }
             else this.gameOver = false
         }
     },
-    //clears all values in preparation for a game
-    initialize: function() {
-        this.deck = []
-        this.p1Deck = []
-        this.p2Deck = []
-        this.kitty = []
-        this.firstCard = null
-        this.secondCard = null
-        this.counter = 0
-        this.numTies = 0
-        this.gameWinner = null
-        this.gameOver = false
-        this.buildDeck()
-        this.shuffle(this.deck)
-        this.dealCards()
-    },
-    printGameState: function() {
-        console.log(`game: ${this.gameNum} counter: ${this.counter}, ${this.firstCard.rank} v ${this.secondCard.rank}, Total Cards: ${this.p1Deck.length + this.p2Deck.length}, p1: ${this.p1Deck.length}, p2: ${this.p2Deck.length}, kitty: ${this.kitty.length},`)
-    },
-    //saves the game information to 'savedGames'
-    logGame: function() {
-        //game, count, p1Length, p2Length, kittyLength
-        let game = new Game(this.gameNum, this.counter, this.p1Deck.length, this.p2Deck.length, this.kitty.length, this.numTies, this.gameWinner)
-        this.savedGames.push(game)
-        this.numTurns += this.counter
-        console.log(JSON.stringify(game))
-    },
-    //function to start the game. Receives # of games & boolean. 
+    
+    //THE GAME WRAPPER:  CALL TO START GAME: Receives # of games & boolean. 
     runGame: function(showTurns=false, numGames=1) {
         //ready game counter
         this.savedGames = []
@@ -195,20 +218,7 @@ const war = {
             this.flipFight(showTurns)
             this.logGame()
         }
-        console.log(`Average # of turns to win over ${numGames} games: ${this.averageCounter/numGames}`)
         this.runStats();
-        console.log(`Game Results:`)
-        console.log(JSON.stringify(this.savedGames))
-    },
-    //generates stats on the game set
-    runStats: function() {
-        let aveTies = Math.floor(this.numTurns/this.numTies)
-        let mean = this.numTurns/this.gameNum
-
-        console.log("GAME SET METRICS")
-        console.log(`GAMES: ${this.gameNum} | TOTAL TURNS ${this.numTurns} | AVE TURNS/GAME ${mean},`)
-        console.log(`TOTAL TIES: ${this.numTies} | AVE TURN PER TIE ${aveTies}`) 
-        
     }
 }
 
